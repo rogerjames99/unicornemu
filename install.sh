d648cb13d99bbf8823832a8b162bd25813df69b9
Roger James <roger@beardandsandals.co.uk>
2015-07-16 09:23:34 +0100

    install changes

--- install.sh
+++ install.sh
@@ -7,10 +7,13 @@ cp colourchooser.desktop /usr/share/applications
 # Icons
 cp unicornemu.png /usr/share/icons/hicolor/48x48/apps
 cp colourchooser.png /usr/share/icons/hicolor/48x48/apps
+gtk-update-icon-cache -f /usr/share/icons/hicolor
 
 # Scripts
 cp unicornemu.py /usr/local/bin/unicornemu
+chmod a+x /usr/local/bin/unicornemu
 cp colourchooser.py /usr/local/bin/colourchooser
+chmod a+x /usr/local/bin/colourchooser
 
 #User interface files
 mkdir /usr/share/unicornemu
@@ -20,3 +23,5 @@ cp colourchooser.ui /usr/share/unicornemu
 # Avahi service def
 cp scratch-remote-sensor.service /etc/avahi/services
 
+# Scratch projects
+cp unicorn.sb "/home/team/Documents/Scratch Projects"

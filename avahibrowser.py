f3cc03b9b2e52b5b73445991b8e405f37cda7e8f
Roger James <roger@beardandsandals.co.uk>
2015-07-21 19:33:19 +0100

    Stuff

--- avahibrowser.py
+++ avahibrowser.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 from gi.repository import Gio, GLib, GObject, Gtk
 import avahi
+import signal
 
 class avahibrowser(Gio.Application):
-    
-    def myhandler(interface, protocol, name, stype, domain, flags):
-        print "Found service '%s' type '%s' domain '%s' " % (name, stype, domain)
+    def service_resolved(self, *args):
+        print 'service resolved'
+        print 'name:', args[2]
+        print 'address:', args[7]
+        print 'port:', args[8]
 
-        avahiserver.ResolveService(interface, protocol, name, stype, 
-            domain, avahi.PROTO_UNSPEC, dbus.UInt32(0), 
-            reply_handler=service_resolved, error_handler=print_error)
+    def print_error(self, *args):
+        print 'error_handler'
+        print args[0]
 
-    def cb(self, proxy, sender, signal, args):
-        ignore = proxy
-        ignore = sender
-        print 'signal', signal, 'arguments', args
+    def browserCallback(self, proxy, sender, signal, args):
+        if signal == 'ItemNew':
+            print "Found service '%s' type '%s' domain '%s' " % (args[2], args[3], args[4])
+            #self.avahiserver.ResolveService('iisssiu',     
+            #    args[0], # Interface
+            #    args[1], # Protocol
+            #    args[2], # Name
+            #    args[3], # Service Type 
+            #    args[4], # Domain
+            #    avahi.PROTO_UNSPEC, dbus.UInt32(0), 
+            #    reply_handler=self.service_resolved, error_handler=self.print_error)
+        else:
+            print 'signal', signal, 'arguments', args
         
-    def do_run(self):
-        print 'Running'
-
     def do_activate(self):
         print 'Activated'
 
@@ -42,62 +51,16 @@ class avahibrowser(Gio.Application):
                                             avahi.DBUS_INTERFACE_SERVICE_BROWSER,
                                             None)
                                             
-        self.avahibrowser.connect('g-signal', self.cb)
-        
-            
+        self.avahibrowser.connect('g-signal', self.browserCallback)
+                    
     # Gnome application initialization routine
     def __init__(self, application_id, flags):
         Gio.Application.__init__(self, application_id=application_id, flags=flags)
 
-
 if __name__ == "__main__":
     # For backwards compatibity
     GObject.threads_init()
-    
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     Application = avahibrowser("uk.co.beardandsandals.avahibrowser", Gio.ApplicationFlags.FLAGS_NONE)
     Application.hold()
     Application.run(None)
-
-
-
-
-'''
-import dbus, gobject, avahi
-from dbus import DBusException
-from dbus.mainloop.glib import DBusGMainLoop
-
-TYPE = '_scratch._tcp'
-
-def service_resolved(*args):
-    print 'service resolved'
-    print 'name:', args[2]
-    print 'address:', args[7]
-    print 'port:', args[8]
-
-def print_error(*args):
-    print 'error_handler'
-    print args[0]
-    
-def myhandler(interface, protocol, name, stype, domain, flags):
-    print "Found service '%s' type '%s' domain '%s' " % (name, stype, domain)
-
-    server.ResolveService(interface, protocol, name, stype, 
-        domain, avahi.PROTO_UNSPEC, dbus.UInt32(0), 
-        reply_handler=service_resolved, error_handler=print_error)
-
-loop = DBusGMainLoop()
-
-bus = dbus.SystemBus(mainloop=loop)
-
-server = dbus.Interface( bus.get_object(avahi.DBUS_NAME, '/'),
-        'org.freedesktop.Avahi.Server')
-
-sbrowser = dbus.Interface(bus.get_object(avahi.DBUS_NAME,
-        server.ServiceBrowserNew(avahi.IF_UNSPEC,
-            avahi.PROTO_UNSPEC, TYPE, 'local', dbus.UInt32(0))),
-        avahi.DBUS_INTERFACE_SERVICE_BROWSER)
-
-sbrowser.connect_to_signal("ItemNew", myhandler)
-
-gobject.MainLoop().run()
-'''
